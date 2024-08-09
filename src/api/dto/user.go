40ba060f5770be12627be38e4189cabea66718df
ehsan hosseiny <ehsanhossini@gmail.com>
2024-08-09 11:54:04 +0330

    added more object according to authentication in dto object

--- src/api/dto/user.go
+++ src/api/dto/user.go
@@ -1,11 +1,29 @@
 package dto
 
 type GetOtpRequest struct {
-	MobileNumber string `json:"mobileNumber" binding:"required,min=11,max=11"`
+	MobileNumber string `json:"mobileNumber" binding:"required,mobile,min=11,max=11"`
 }
 type TokenDetail struct {
 	AccessToken            string `json:"accessToken"`
 	RefreshToken           string `json:"refreshToken"`
 	AccessTokenExpireTime  int64  `json:"accessTokenExpireTime"`
 	RefreshTokenExpireTime int64  `json:"refreshTokenExpireTime"`
+}
+
+type RegisterUserByUsername struct {
+	FirstName string `json:"firstName" binding:"required,min=3"`
+	LastName  string `json:"lastName" binding:"required,min=6"`
+	Username  string `json:"userName" binding:"required,min=5"`
+	Email     string `json:"email" binding:"required,email"`
+	Password  string `json:"password" binding:"required,,password,min=3"`
+}
+
+type RegisterLoginByMobile struct {
+	MobileNumber string `json:"mobileNumber" binding:"required,mobile,min=11,max=11"`
+	Otp          string `json:"otp" binding:"required,min=6,max=6"`
+}
+
+type LoginByUsername struct {
+	Username string `json:"userName" binding:"required,min=6"`
+	Password string `json:"password" binding:"required,password,min=6"`
 }
