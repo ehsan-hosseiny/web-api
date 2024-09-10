8c38176f4f44462af74eb2ead11660b3478e0910
pc <ehsanhossini@gmail.com>
2024-09-10 11:40:37 +0330

    added propery category and property crud

--- src/api/api.go
+++ src/api/api.go
@@ -7,10 +7,13 @@ import (
 	"github.com/ehsan-hosseiny/golang-web-api/api/routers"
 	"github.com/ehsan-hosseiny/golang-web-api/config"
 	"github.com/ehsan-hosseiny/golang-web-api/docs"
+	"github.com/ehsan-hosseiny/golang-web-api/pkg/logging"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+var logger = logging.NewLogger(config.GetConfig())
 
 func InitServer(cfg *config.Config) {
 	r := gin.New()
@@ -37,6 +40,10 @@ func InitServer(cfg *config.Config) {
 	// }
 
 	r.Run(fmt.Sprintf(":%s", cfg.Server.Port))
+	err := r.Run(fmt.Sprintf(":%s", cfg.Server.Port))
+	if err != nil {
+		logger.Fatal(logging.General, logging.Startup, err.Error(), nil)
+	}
 }
 
 func RegisterRoutes(r *gin.Engine, cfg *config.Config) {
@@ -50,12 +57,15 @@ func RegisterRoutes(r *gin.Engine, cfg *config.Config) {
 
 		// User
 		users := v1.Group("/users")
-		
+
 		// Base
 		countries := v1.Group("/countries", middlewares.Authentication(cfg), middlewares.Authorization([]string{"admin"}))
 		cities := v1.Group("/cities", middlewares.Authentication(cfg), middlewares.Authorization([]string{"admin"}))
 		files := v1.Group("/files", middlewares.Authentication(cfg), middlewares.Authorization([]string{"admin"}))
 
+		// Property
+		properties := v1.Group("/properties", middlewares.Authentication(cfg), middlewares.Authorization([]string{"admin"}))
+		propertyCategories := v1.Group("/property-categories", middlewares.Authentication(cfg), middlewares.Authorization([]string{"admin"}))
 
 		// Test
 		routers.Health(health)
@@ -68,6 +78,10 @@ func RegisterRoutes(r *gin.Engine, cfg *config.Config) {
 		routers.Country(countries, cfg)
 		routers.City(cities, cfg)
 		routers.File(files, cfg)
+
+		// Property
+		routers.Property(properties, cfg)
+		routers.PropertyCategory(propertyCategories, cfg)
 
 	}
 
---
+++ src/api/routers/properties.go
@@ -0,0 +1,27 @@
+package routers
+
+import (
+	"github.com/ehsan-hosseiny/golang-web-api/api/handlers"
+	"github.com/ehsan-hosseiny/golang-web-api/config"
+	"github.com/gin-gonic/gin"
+)
+
+func PropertyCategory(r *gin.RouterGroup, cfg *config.Config) {
+	h := handlers.NewPropertyCategoryHandler(cfg)
+
+	r.POST("/", h.Create)
+	r.PUT("/:id", h.Update)
+	r.DELETE("/:id", h.Delete)
+	r.GET("/:id", h.GetById)
+	r.POST("/get-by-filter", h.GetByFilter)
+}
+
+func Property(r *gin.RouterGroup, cfg *config.Config) {
+	h := handlers.NewPropertyHandler(cfg)
+
+	r.POST("/", h.Create)
+	r.PUT("/:id", h.Update)
+	r.DELETE("/:id", h.Delete)
+	r.GET("/:id", h.GetById)
+	r.POST("/get-by-filter", h.GetByFilter)
+}
